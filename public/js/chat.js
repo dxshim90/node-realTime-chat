3c2c00c227c5e29f847b2f9cf964c4af5764c704
dxshim90 <dxshim90@gmail.com>
2019-08-18 13:19:24 +0100

    websockets implemented

---
+++ public/js/chat.js
@@ -0,0 +1,16 @@
+const socket = io();
+
+socket.on("welcomeMessage", welcomeMessage => {
+  console.log(welcomeMessage);
+});
+
+socket.on("message", message => {
+  console.log(message);
+});
+
+const form = document.querySelector("#message-form");
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const message = e.target.elements.message.value;
+  socket.emit("sendMessage", message);
+});
--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const PORT = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
+// let count = 0;
+io.on("connection", socket => {
+  console.log("New Socket Connction");
 
-app.listen(PORT, () => {
+  socket.emit("welcomeMessage", "Welcome!");
+
+  socket.on("sendMessage", message => {
+    io.emit("message", message);
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
